5e3d85f53c192b29ba5c07a8f6f19ef4d1f0207b
alexeysvatov <zombeaver@gmail.com>
2022-10-09 10:38:40 +0500

    correct task_6 with chardet

--- Svatov_Alexey_dz_1/task_6.py
+++ Svatov_Alexey_dz_1/task_6.py
@@ -1,5 +1,11 @@
-import locale
+from chardet import detect
 
-default_encoding = locale.getpreferredencoding()
-with open('test_file.txt', 'r', encoding=default_encoding) as test_file:
-    print(test_file.read())
+with open('test_file.txt', 'rb') as test_file:
+    content = test_file.read()
+
+encoding = detect(content)['encoding']
+print(encoding)
+
+with open('test_file.txt', 'r', encoding=encoding) as test_file:
+    content = test_file.read()
+print(content)
