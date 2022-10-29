0af054cd31725205816073840e82bb9200af2fdb
alexeysvatov <zombeaver@gmail.com>
2022-10-29 11:35:45 +0500

    correct code with examples

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
-import json
+import select
 import sys
-from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
+from datetime import time
+from socket import socket, AF_INET, SOCK_STREAM
 import logging
 from logs.config import server_log_config
 
 from common.variables import ACTION, ACCOUNT_NAME, RESPONSE, MAX_CONNECTIONS, \
-    PRESENCE, TIME, USER, ERROR, DEFAULT_PORT
+    PRESENCE, TIME, USER, ERROR, DEFAULT_PORT, MESSAGE, SENDER, MESSAGE_TEXT
 from common.utils import get_message, send_message
 from common.decos import Log
 
@@ -63,20 +64,57 @@ def main():
 
     transport = socket(AF_INET, SOCK_STREAM)
     transport.bind((listen_address, listen_port))
-    transport.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    transport.settimeout(0.5)
+
+    clients = []
+    messages = []
+
     transport.listen(MAX_CONNECTIONS)
 
     while True:
-        client, client_address = transport.accept()
         try:
-            message_from_client = get_message(client)
-            logger.info(f'CLIENT MESSAGE: {message_from_client}')
-            response = process_client_message(message_from_client)
-            send_message(client, response)
-            client.close()
-        except (ValueError, json.JSONDecodeError):
-            logger.critical('Принято некорректное сообщение от клиента')
-            client.close()
+            client, client_address = transport.accept()
+        except OSError as err:
+            print(err.errno)  # The error number returns None because it's just a timeout
+            pass
+        else:
+            clients.append(client)
+
+        recv_data_lst = []
+        send_data_lst = []
+        err_lst = []
+        # Проверяем на наличие ждущих клиентов
+        try:
+            if clients:
+                recv_data_lst, send_data_lst, err_lst = select.select(clients, clients, [], 0)
+        except OSError:
+            pass
+
+        # принимаем сообщения и если там есть сообщения,
+        # кладём в словарь, если ошибка, исключаем клиента.
+        if recv_data_lst:
+            for client_with_message in recv_data_lst:
+                try:
+                    process_client_message(get_message(client_with_message),
+                                           messages, client_with_message)
+                except:
+                    clients.remove(client_with_message)
+
+        # Если есть сообщения для отправки и ожидающие клиенты, отправляем им сообщение.
+        if messages and send_data_lst:
+            message = {
+                ACTION: MESSAGE,
+                SENDER: messages[0][0],
+                TIME: time.time(),
+                MESSAGE_TEXT: messages[0][1]
+            }
+            del messages[0]
+            for waiting_client in send_data_lst:
+                try:
+                    send_message(waiting_client, message)
+                except:
+                    waiting_client.close()
+                    clients.remove(waiting_client)
 
 
 if __name__ == '__main__':
